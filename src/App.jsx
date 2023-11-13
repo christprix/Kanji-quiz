9489e734ea51e00285cc44dfa1d55eaf42e4f169
Christian Stprix <christprix@gmail.com>
2023-11-13 09:38:19 -0500

    added submit button

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import Flashcard from "./Flashcard";
+import Message from "./Message"
 import axios from 'axios'
 
 function App() {
   const [kanji, setKanji] = useState([])
+  const [message, setMessage] = useState()
 
   function decodeString(str) {
     const textArea = document.createElement('textarea')
     textArea.innerHTML = str
     return textArea.value
   }
-  useEffect(() => {
+  // useEffect(() => {
+  //   axios.get('https://kanjiapi.dev/v1/kanji/kyoiku')
+  //     .then(res => {
+  //       let num = Math.floor(Math.random() * 80)
+  //       function getnum() {
+  //         return Math.floor(Math.random() * 80)
+  //       }
+  //       axios.get(`https://kanjiapi.dev/v1/kanji/${res.data[`${num}`]}`)
+  //         .then(response => {
+  //           let kanji_data = response.data
+  //           setKanji([{
+  //             id: kanji_data.unicode,
+  //             character: kanji_data.kanji,
+  //             meaning: decodeString(kanji_data.meanings['0']),
+  //             options: [res.data[getnum()], res.data[getnum()], res.data[getnum()], kanji_data.kanji]
+  //           }
+  //           ]);
+  //         })
+  //     })
+  // }, [])
+
+  function start(e) {
     axios.get('https://kanjiapi.dev/v1/kanji/kyoiku')
       .then(res => {
         let num = Math.floor(Math.random() * 80)
@@ -30,15 +53,17 @@ function App() {
             ]);
           })
       })
-  }, [])
+  }
 
   return (
     <>
       <div className='container'>
-        <h1>Become a Kanji King!</h1>
+        <h1>Are you smarter than a (Japanese) 5th grader?</h1>
+        <button className="btn" onClick={start}>Ready?</button>
         {kanji.map(k => {
-          return <Flashcard kanji={k} key={k.id} />
+          return <Flashcard kanji={k} key={k.id} message={setMessage} start={start} />
         })}
+        <Message message={message} />
       </div>
     </>
   )
--- src/Flashcard.jsx
+++ src/Flashcard.jsx
@@ -1,14 +1,21 @@
 import React, { useState } from 'react'
 
-export default function Flashcard({ kanji }) {
+export default function Flashcard({ kanji, message, start }) {
   const [flip, setFlip] = useState(false)
 
-  function handleClick(e) {
-    if (e === kanji.character) {
+  const handleClick = event => {
+    console.log(event.currentTarget.innerHTML);
+    console.log(kanji.character);
+    if (event.currentTarget.innerHTML === kanji.character) {
       console.log("Right answer!");
+      message("Right Answer")
+      start()
     }
     else {
       console.log('wrong answer!');
+      message("Wrong Answer")
+      event.currentTarget.classList.add("wrong")
+      start()
     }
   }
   return (
@@ -22,8 +29,8 @@ export default function Flashcard({ kanji }) {
         </div>
       </div>
       <div className='options' >{kanji.options.sort(() => Math.random() - 0.5).map(option => {
-        return <div className='card'
-          onClick={() => handleClick(option)} key={option}>
+        return <div className='card option'
+          onClick={handleClick} key={option}>
           {option}
         </div>
       })}</div>
